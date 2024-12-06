d824d439f6128ef6b0805eece147ddf3e10454d5
oluoko <brianotieno586@gmail.com>
2024-12-06 14:43:51 +0300

    Movie Review: Initalized MovieRepository Interface

---
+++ movie-review/src/main/java/dev/scene_it_all/movie_review/MovieRepository.java
@@ -0,0 +1,9 @@
+package dev.scene_it_all.movie_review;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+}
