1107a9d8a31968c6b1e0f51fbc61eccd2a7bd5e9
oluoko <brianotieno586@gmail.com>
2024-12-06 14:48:56 +0300

    Movie Review: Initialized MovieService Class

---
+++ movie-review/src/main/java/dev/scene_it_all/movie_review/MovieService.java
@@ -0,0 +1,15 @@
+package dev.scene_it_all.movie_review;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+}
